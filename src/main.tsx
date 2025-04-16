99ff6efb530fe4a31b353ba5ded19f94401c37f8
Tasfiqul Asif <tasfiq.asif.23@gmail.com>
2025-04-16 20:00:36 +0600

    deployed link added

--- src/main.tsx
+++ src/main.tsx
@@ -10,15 +10,6 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { SidebarProvider } from "./components/ui/sidebar";
 
-// Handle redirect from 404 page
-(function () {
-  const redirect = new URLSearchParams(window.location.search).get("redirect");
-  if (redirect) {
-    // Remove the query parameter and replace with the original path
-    history.replaceState(null, "", "/" + redirect);
-  }
-})();
-
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
